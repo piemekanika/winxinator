a7d4b4e7709a2806fc59c16d8553681ef8e421e7
piemekanika@hotmail.com <piemekanika@hotmail.com>
2020-05-10 21:00:54 +0700

    initial

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>testing the speed</h1>
+  
+  <p>Edit ./App.vue to test hot module replacement (HMR).</p>
+  <p>
+    <span>Count is: {{ count }}</span>
+    <button @click="count++">increment</button>
+  </p>
+</template>
+
+<script>
+export default {
+  data: () => ({ count: 1 })
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #4fc08d;
+}
+
+h1, p {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
